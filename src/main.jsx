e5405692a7cb292ca08f727a0d586e2bbfd8bb1e
jonathanespana <tempe.espana@gmail.com>
2024-01-19 11:34:51 -0700

    configured react-router-dom and materialui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom'
 import './index.css'
+import Root from "./routes/root"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+    >
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/root.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+export default function Root() {
+
+    return (
+        <>
+            <div id="navbar">
+                <Button variant="contained">Hello world</Button>
+            </div>
+        </>
+    );
+}
